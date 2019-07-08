9cd660dd501650fe6be7a98dfa016c72f0e74293
Guillaume <canac78170@aol.com>
2019-07-08 11:14:32 +0200

    Guillaume 08/07/2019 Fonction rajout utilisateur + mdp

---
+++ usradd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Fonction création utilisateur et mot de passe
+
+add_user () {
+
+# Condition de sortie de la boucle est la saisie obligatoire (caractères spéciaux, chiffres inclus.
+
+username=""
+until [ -n "$username" ]
+  do
+    read -p "Veuillez saisir un nom d'utilisateur: " username
+done
+
+# création d'un utilisateur et de son mot de passe (pas d'obligation longeur mdp) 
+
+adduser $username
+passwd $username
+}
+
+add_user
