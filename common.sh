13ad84dafe1bb750e48c3368c7e0aecf6f8309c9
Aurélie Samyn <aurelie.samyn@gmail.com>
2019-07-09 21:46:13 +0200

    Modification du test sudoer (pas d'utilisation de fichier err)

--- common.sh
+++ common.sh
@@ -16,11 +16,6 @@ function test_sudoer ()
 {
 
   # On redirige la sortie de la commande sudo -l dans le fichier err
-  sudo -l > err 2>&1
-
-  # Si le fichier err contient le mot "Sorry", l'utilisateur n'a pas les privilèges administrateur
-  # Dans ce cas, la fonction renvoie 0, sinon elle renvoie 1
-  # Le résultat est stocké dans la variable $sudoer
-  [ -n "$(cat err | grep Sorry)" ] && sudoer=false || sudoer=true
+  timeout 2 sudo id > /dev/null 2>&1 && sudoer="true" || sudoer="false"
 
 }
