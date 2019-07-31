aaeaf6513b8fdbbfb479326ce468f776383d273b
Aurélie Samyn <aurelie.samyn@gmail.com>
2019-07-31 20:01:26 +0200

    [IMP] sudobox.sh : sécurisation du script
    - Déconnexion du sudo dès le lancement
    - Exécution dans un sous-shell
    - Nettoyage du code

--- sudobox.sh
+++ sudobox.sh
@@ -5,22 +5,30 @@
 #                #
 ##################
 
-# Inclusion des scripts
-# On recherche tous les scripts .sh et on les inclut (sauf le script principal sudobox.sh)
-# Exclusion temporaire du dossier files
-for f in `find . -name "*.sh" -type f` ; do
-  [ $f != "./sudobox.sh" ] && [[ $f != *files* ]] && source $f
-done
+# Sécurisation du script : déconnexion du sudo
+sudo -k
 
-clear
-blue_bold "------------------------------------"
-blue_bold "- Bienvenue dans la boîte à outils -"
-blue_bold "------------------------------------"
-echo
+# Exécution dans un sous-shell
+(
+  # Inclusion des scripts
+  # On recherche tous les scripts .sh et on les inclut (sauf le script principal sudobox.sh)
+  # Exclusion temporaire du dossier files
+  for f in $(find . -name "*.sh" -type f); do
+    [[ "$f" != "./sudobox.sh" ]] && [[ "$f" != *files* ]] && source "$f"
+  done
 
-# Affichage du menu de choix du mode d'utilisation de la boîte à outils (utilisateur ou sudoer)
-menu_mode
-# Affichage du menu principal menus.sh
-menu_main
+  clear
+  blue_bold "------------------------------------"
+  blue_bold "- Bienvenue dans la boîte à outils -"
+  blue_bold "------------------------------------"
+  echo
+
+  # Affichage du menu de choix du mode d'utilisation de la boîte à outils (utilisateur ou sudoer)
+  menu_mode
+  # Affichage du menu principal menus.sh
+  menu_main
+)
+# Fin du sous-shell
+
 # Sécurisation du script : déconnexion du sudo
 sudo -k
