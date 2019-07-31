0fd66a7ea85bce3852aa794c10fd81bca3defcc6
Aurélie Samyn <aurelie.samyn@gmail.com>
2019-07-31 20:09:59 +0200

    [IMP] common/global_var.sh : variables en lecture seule

--- common/global_var.sh
+++ common/global_var.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # ID min et max pour les groupes d'utilisateurs réservés au système
-sysgidmin=$(grep ^SYS_GID_MIN /etc/login.defs | awk '{print $2}')
-sysgidmax=$(grep ^SYS_GID_MAX /etc/login.defs | awk '{print $2}')
+readonly sysgidmin=$(grep ^SYS_GID_MIN /etc/login.defs | awk '{print $2}')
+readonly sysgidmax=$(grep ^SYS_GID_MAX /etc/login.defs | awk '{print $2}')
 # ID min et max pour les groupes d'utilisateurs normaux (créés avec groupadd)
-gidmin=$(grep ^GID_MIN /etc/login.defs | awk '{print $2}')
-gidmax=$(grep ^GID_MAX /etc/login.defs | awk '{print $2}')
+readonly gidmin=$(grep ^GID_MIN /etc/login.defs | awk '{print $2}')
+readonly gidmax=$(grep ^GID_MAX /etc/login.defs | awk '{print $2}')
 
 # ID min et max pour les utilisateurs réservés au système
-sysuidmin=$(grep ^SYS_UID_MIN /etc/login.defs | awk '{print $2}')
-sysuidmax=$(grep ^SYS_UID_MAX /etc/login.defs | awk '{print $2}')
+readonly sysuidmin=$(grep ^SYS_UID_MIN /etc/login.defs | awk '{print $2}')
+readonly sysuidmax=$(grep ^SYS_UID_MAX /etc/login.defs | awk '{print $2}')
 # ID min et max pour les utilisateurs normaux (créés avec useradd)
-uidmin=$(grep ^UID_MIN /etc/login.defs | awk '{print $2}')
-uidmax=$(grep ^UID_MAX /etc/login.defs | awk '{print $2}')
+readonly uidmin=$(grep ^UID_MIN /etc/login.defs | awk '{print $2}')
+readonly uidmax=$(grep ^UID_MAX /etc/login.defs | awk '{print $2}')
